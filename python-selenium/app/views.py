5feb9fae4ff3200b4ef6c0bfca67779877688bb1
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-12-16 18:50:31 +0900

    base

--- python-selenium/app/views.py
+++ python-selenium/app/views.py
@@ -1,3 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/index.html')
